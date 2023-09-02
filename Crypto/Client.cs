e8fd5db01da83cbf8ca7698cffa5944533e7ad73
Yn1t <yn1tishe@gmail.com>
2023-09-02 13:10:50 +0300

    Добавьте файлы проекта.

---
+++ Crypto/Client.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Security.Cryptography;
+using Benalo.classes;
+using Benalo.interfaces;
+using filemanager;
+using Magenta;
+
+namespace Crypto
+{
+    class Client
+    {
+        private TcpClient? client = new TcpClient();
+
+        private string host = string.Empty;
+        private int port = 0;
+        private byte[] sessionKey;
+
+        public FileEncryptor encryptor = new FileEncryptor();
+        public FileDecryptor decryptor = new FileDecryptor();
+
+        public void connect(string host, int port)
+        {
+            if (client == null)
+                client = new TcpClient();
+
+            if (client.Connected)
+            {
+                disconnect();
+                client = new TcpClient(host, port);
+            }
+            else
+            {
+                try
+                {
+                    client.Connect(host, port);
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            if (!client.Connected)
+                return;
+
+            var stream = client.GetStream();
+            var binaryReader = new BinaryReader(stream);
+
+            string str = binaryReader.ReadString();
+            while (str != "key")
+            { }
+
+            Key key = new Key();
+            int size = binaryReader.ReadInt32();
+            key.y = new BigInteger(binaryReader.ReadBytes(size));
+            size = binaryReader.ReadInt32();
+            key.r = new BigInteger(binaryReader.ReadBytes(size));
+            size = binaryReader.ReadInt32();
+            key.n = new BigInteger(binaryReader.ReadBytes(size));
+
+            var benalohClass = new AsymmetricEncryptionDecryption(key);
+
+            var rnd = new Random();
+
+            var bytes = new Byte[16];
+
+            rnd.NextBytes(bytes);
+
+            var binaryWriter = new BinaryWriter(stream);
+            binaryWriter.Write("sessionKey");
+
+            for (int i = 0; i < bytes.Length; i += 2)
+            {
+                var temp = bytes.ToList().GetRange(i, 2);
+                var _sessionKey = new BigInteger(temp.ToArray(), true);
+
+                var encryptedKey = benalohClass.Encryption(_sessionKey);
+                size = encryptedKey.GetByteCount();
+                binaryWriter.Write(size);
+                binaryWriter.Write(encryptedKey.ToByteArray());
+            }
+
+            binaryWriter.Flush();
+
+            sessionKey = bytes;
+        }
+
+        public bool reconnect()
+        {
+            if (host == string.Empty || port == 0)
+            {
+                return false;
+            }
+            else
+            {
+                connect(host, port);
+                return true;
+            }
+        }
+
+        public bool isConnected()
+        {
+            if (client != null)
+                return client.Connected;
+            else
+                return false;
+        }
+
+        public void disconnect()
+        {
+            client.Close();
+            client = null;
+        }
+
+        public bool sendFile(string filePath)
+        {
+            if (client.Connected)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                if (fileInfo.Exists)
+                {
+                    encryptor.Encrypt(filePath, "encrypted", sessionKey);
+                    FileInfo encryptedInfo = new FileInfo("encrypted");
+                    var size = encryptedInfo.Length;
+
+                    var stream = client.GetStream();
+                    var binaryWriter = new BinaryWriter(stream);
+
+                    binaryWriter.Write("file");
+                    binaryWriter.Write(fileInfo.Name);
+                    binaryWriter.Write(size);
+                    binaryWriter.Write(encryptor.paddingCount);
+
+                    FileStream f_in = new FileStream("encrypted", FileMode.Open);
+                    for (int i = 0; i < size / 16; ++i)
+                    {
+                        byte[] bytes = new byte[16];
+                        f_in.Read(bytes, 0, bytes.Length);
+                        binaryWriter.Write(bytes);
+                    }
+                    f_in.Close();
+
+                    File.Delete("encrypted");
+
+                    var binaryReader = new BinaryReader(stream);
+                    //binaryWriter.Flush();
+                    if (binaryReader.ReadString() == "OK")
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public bool getFile(string fileName)
+        {
+            if (client != null && client.Connected)
+            {
+                var stream = client.GetStream();
+                var binaryWriter = new BinaryWriter(stream);
+                var binaryReader = new BinaryReader(stream);
+
+                binaryWriter.Write("down");
+                binaryWriter.Write(fileName);
+
+                string name = binaryReader.ReadString();
+
+                if (name == "Dont")
+                    return false;
+
+                int size = (int)binaryReader.ReadInt64();
+                int padding = binaryReader.ReadInt32();
+
+                File.Delete("encrypted");
+                File.Delete(name);
+                FileStream f_in = new FileStream("encrypted", FileMode.Create);
+
+                for (int i = 0; i < size / 16; ++i)
+                {
+                    byte[] bytes = binaryReader.ReadBytes(16);
+                    f_in.Write(bytes);
+                }
+                f_in.Close();
+
+                decryptor.Decrypt("encrypted", fileName, sessionKey, padding);
+                File.Delete("encrypted");
+                binaryWriter.Write("OK");
+
+                Console.WriteLine($"File {name} downloaded to server");
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Crypto/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Crypto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Client client = new();
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 5);
+            dispatcherTimer.Start();
+        }
+
+        private void fileChooseButtob_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                fileNames.Items.Add(openFileDialog.FileName);
+        }
+
+        private void chooseFileToDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var deletedItem = fileNames.SelectedItem;
+
+            if (deletedItem != null)
+            {
+                fileNames.Items.Remove(deletedItem);   
+            }
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (connectState.IsChecked == false)
+            {
+                connectState.IsChecked = true;
+                connectState.Background = new SolidColorBrush(Colors.Green);
+                connectState.Content = "Подключение...";
+                connectButton.Content = "";
+
+                client.connect("localhost", 8000);
+
+                connectState.IsChecked = true;
+                connectState.Background = new SolidColorBrush(Colors.Yellow);
+                connectState.Content = "Подключено";
+                connectButton.Content = "Отключиться";
+
+                if (!client.isConnected())
+                {
+                    connectState.IsChecked = false;
+                    connectState.Background = null;
+                    connectState.Content = "Нет соединения";
+                    connectButton.Content = "Подключиться";
+                    client.disconnect();
+                }
+                // TODO connect method
+            }
+            else
+            {
+                connectState.IsChecked = false;
+                connectState.Background = null;
+                connectState.Content = "Нет соединения";
+                connectButton.Content = "Подключиться";
+
+                client.disconnect();
+                // TODO disconnect method
+            }
+        }
+
+        private async void sendFile_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() => sendFiles());
+        }
+
+        private void sendFiles()
+        {
+            foreach (string filePath in fileNames.Items)
+            {
+                client.sendFile(filePath);
+            }
+        }
+
+        private async void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (client.isConnected())
+            {
+                progressBar.Value = client.encryptor.process;
+            }
+        }
+
+        private void downloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (client.getFile(downloadFileName.Text))
+                downloadStatus.Content = "Загружено";
+            else
+                downloadStatus.Content = "Не удалость загрузить файл";
+        }
+    }
+}
